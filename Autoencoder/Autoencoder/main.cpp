8842d0ac687e9066a55353f9ab8338340ecd3f6c
Nick Everitt <nicholaseveritt@gmail.com>
2020-01-01 09:52:29 +0000

    Create Xcode project

---
+++ Autoencoder/Autoencoder/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Autoencoder
+//
+//  Created by Nick on 01/01/2020.
+//  Copyright © 2020 Nick. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
